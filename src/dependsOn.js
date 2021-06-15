ec43992d1f31b0cadf24ea6257debc9ac1ec55ff
Simon Ng <simongcc@gmail.com>
2021-06-15 11:58:23 +0800

    update with anonymous function wrapper supporting jQuery noConflict mode

--- src/dependsOn.js
+++ src/dependsOn.js
@@ -1,32 +1,34 @@
-/*!
- * dependsOn v${version}
- * a jQuery plugin to facilitate the handling of form field dependencies.
- *
- * Copyright 2016 David Street
- * Licensed under the MIT license.
- */
+(function( $ ) {
+	/*!
+	 * dependsOn v${version}
+	 * a jQuery plugin to facilitate the handling of form field dependencies.
+	 *
+	 * Copyright 2016 David Street
+	 * Licensed under the MIT license.
+	 */
+	var SubjectController = require('./subject-controller')
+	
+	/**
+	 * Plugin access point.
+	 * @param {Object} initialSet An object of key-value pairs of selectors and qualifiers
+	 * representing the inital DependencySet.
+	 * @param {Object} opts An object for key-value pairs of options.
+	 * @return {SubjectController}
+	 */
+	$.fn.dependsOn = function(initialSet, opts) {
+		var options = $.extend({}, {
+			disable: true,
+			hide: true,
+			duration: 200,
+			trigger: 'change'
+		}, opts)
 
-var SubjectController = require('./subject-controller')
+		// Namespace the trigger event
+		options.trigger += (options.trigger.search('.dependsOn') > -1) ? '' :  '.dependsOn'
 
-/**
- * Plugin access point.
- * @param {Object} initialSet An object of key-value pairs of selectors and qualifiers
- * representing the inital DependencySet.
- * @param {Object} opts An object for key-value pairs of options.
- * @return {SubjectController}
- */
-$.fn.dependsOn = function(initialSet, opts) {
-	var options = $.extend({}, {
-		disable: true,
-		hide: true,
-		duration: 200,
-		trigger: 'change'
-	}, opts)
+		var controller = new SubjectController(this, initialSet, options)
 
-	// Namespace the trigger event
-	options.trigger += (options.trigger.search('.dependsOn') > -1) ? '' :  '.dependsOn'
+		return controller
+	}
+}( jQuery ));
 
-	var controller = new SubjectController(this, initialSet, options)
-
-	return controller
-}
